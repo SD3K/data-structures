a24a61c003697faacf8770a4e04fb06fa4031652
Skipper Harris <skipdawg3000@gmail.com>
2022-01-05 16:27:15 -0800

    passing prototypal/stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.index = 0;
+  instance.storage = {};
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.index++] = value;
+    return this.index;
+  },
+  pop: function() {
+    if (this.index > 0) {
+      var popped = this.storage[--this.index];
+      delete this.storage[this.index];
+    }
+    return popped;
+  },
+  size: function() {
+    return this.index;
+  }
+};
 
 
