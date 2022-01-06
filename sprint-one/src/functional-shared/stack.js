60c0f35e4130a000e7fe49b7bb2ae8dc818660bd
Skipper Harris <skipdawg3000@gmail.com>
2022-01-05 16:13:22 -0800

    passing functional-shared/stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    index: 0,
+    storage: {}
+  };
+
+  return _.extend(instance, stackMethods);
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.index++] = value;
+    return this.index;
+  },
+  pop: function() {
+    if (this.index > 0) {
+      var popped = this.storage[--this.index];
+      delete this.storage[this.index];
+    }
+    return popped;
+  },
+  size: function() {
+    return this.index;
+  }
+};
