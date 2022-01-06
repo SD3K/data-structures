db4167ef83995654b2f86dd7fef4e62849f4d965
Skipper Harris <skipdawg3000@gmail.com>
2022-01-05 16:50:42 -0800

    passing pseudoclassical/stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.index = 0;
+  this.storage = {};
 };
 
-
+Stack.prototype.push = function(value) {
+  this.storage[this.index++] = value;
+  return this.index;
+};
+Stack.prototype.pop = function(value) {
+  if (this.index > 0) {
+    var popped = this.storage[--this.index];
+    delete this.storage[this.index];
+  }
+  return popped;
+};
+Stack.prototype.size = function(value) {
+  return this.index;
+};
